ac8340c4a87dbef77a0b59e7c902e15a72f9ae84
maria <44531542+morshielt@users.noreply.github.com>
2020-11-27 17:54:24 +0100

    input parsing

--- game_of_life/gamemain.c
+++ game_of_life/gamemain.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,120 +6,92 @@
 extern void start(int cols, int rows, char* T);
 extern void run(int steps);
 
-int ctr = 0;
+void print(char* T, int size, int cols, int shift) {
+    size_t i = 0 + shift * size / 2;
+    size -= (1 - shift) * size / 2;
 
-void start2222222(int cols, int rows, char* T) {}
-void run22222222(int steps, int cols, int rows, char* T, int size) {
-    for (size_t step = 0; step < steps; step++) {
-        int from = (ctr % 2) * cols * rows;
-        // printf("from: %d\n", from);
-        int to = ((from + 1) % 2) * cols * rows;
-        // printf("to: %d\n", to);
-
-        for (size_t r = 0; r < rows; r++) {
-            for (size_t c = 0; c < cols; c++) {
-                int neighbours = 0;
-
-                for (int j = -1; j <= 1; j++) {
-                    for (int i = -1; i <= 1; i++) {
-                        if (c + i >= 0 && c + i < cols && r + j >= 0 &&
-                            r + j < rows) {
-                            if (T[(r + j) * cols + (c + i) + from] == '1')
-                                neighbours++;
-                        }
-                    }
-                }
-
-                if (T[r * cols + c + from] == '1') neighbours--;
-                // printf("%d\n", neighbours);
-                if (neighbours == 3 ||
-                    (neighbours == 2 && T[r * cols + c + from] == '1')) {
-                    T[r * cols + c + to] = '1';
-                } else {
-                    T[r * cols + c + to] = '0';
-                }
-            }
-        }
-
-        ctr++;
-    }
-}
-
-void print(char* T, int size, int cols) {
-    printf("\n ");
-    for (size_t i = 0; i < size; i++) {
-        if (i % cols == 0 && i != 0) {
+    printf(" ");
+    for (i; i < size; i++) {
+        if (i % cols == 0 && i != 0 && i != size / 2) {
             printf("\n ");
         }
-        if (i == (size / 2)) {
-            printf("\n ");
-        }
+
         if (T[i] == '0')
-            printf("\x1B[0m∙ \033[0m");
+            printf("∙ ");
         else
             printf("\x1B[36m▪ \033[0m");
-        // printf("%c ", T[i]);
     }
-    printf("\n-----------------------------------\n");
+    printf("\n");
 }
 
 int main(int argc, char* argv[]) {
-    int cols = 9, rows = 9;
-    // int cols = 3, rows = 3;
+    // input file handling
+    FILE* file;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("Error opening file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    // reading number of columns and rows
+    int cols, rows;
+
+    if (fscanf(file, "%d%d", &cols, &rows) != 2) {
+        printf("Error reading number of columns and rows");
+        exit(EXIT_FAILURE);
+    }
+
+    // T contains 2 boards one after another
+    // boards are used alternately:
+    // if 1st is current, then 2nd is next
+    // if 2nd is current, then 1st is next
     int size = cols * rows * 2;
     char T[size];
-    // = {'0'};
 
-    for (size_t i = 0; i < size; i++) {
+    // reading the board
+    for (int i = 0; i < size / 2; i++) {
+        int cell;
+        if (fscanf(file, "%d", &cell) != 1) {
+            printf("Error reading row %d\n", (i % cols + 1));
+            exit(EXIT_FAILURE);
+        }
+        T[i] = cell + '0';
+    }
+    fclose(file);
+
+    // cleaning the 2nd board
+    for (int i = size / 2; i < size; i++) {
         T[i] = '0';
     }
 
-    T[3 * rows + 3] = '1';
-    T[3 * rows + 5] = '1';
-    T[4 * rows + 3] = '1';
-    T[4 * rows + 4] = '1';
-    T[4 * rows + 5] = '1';
-    T[5 * rows + 4] = '1';
-    // T[3] = '1';
-    // T[4] = '1';
-    // T[5] = '1';
+    int ctr = 0, steps;
+    char buffer[16];
+    int STOP = -1;
 
-    print(T, size, cols);
     start(cols, rows, T);
-    for (size_t i = 0; i < 5; i++) {
-        run(2);  // TODO: check run(0);
-        print(T, size, cols);
+    print(T, size, cols, ctr);
+
+    // reading number of steps from input
+    // and priniting according board
+    while (1) {
+        printf("[quit `%d`] [<n> steps `n`] ", STOP);
+
+        if (fgets(buffer, sizeof(buffer), stdin) == NULL) break;
+
+        steps = strtol(buffer, NULL, 10);
+        if (steps == STOP) break;
+        if (steps != 0) {
+            run(steps);
+            ctr = (ctr + steps) % 2;
+            print(T, size, cols, ctr);
+        }
     }
 
-    //     FILE *stream;
-    //     char *line = NULL;
-    //     size_t len = 0;
-    //     ssize_t nread;
-
-    //     if (argc != 2) {
-    //         fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-    //         exit(EXIT_FAILURE);
-    //     }
-
-    //     stream = fopen(argv[1], "r");
-    //     if (stream == NULL) {
-    //         perror("fopen");
-    //         exit(EXIT_FAILURE);
-    //     }
-
-    //     const int MAX_NUM_LEN = 128;
-    //     char cols[MAX_NUM_LEN], rows[MAX_NUM_LEN];
-    //     int cols, rows;
-    //     if ((nread = getline(&line, &len, stream)) != -1) {
-    //     }
-
-    //     while ((nread = getline(&line, &len, stream)) != -1) {
-    //         printf("Retrieved line of length %zd:\n", nread);
-    //         fwrite(line, nread, 1, stdout);
-    //     }
-
-    //     free(line);
-    //     fclose(stream);
-    //     exit(EXIT_SUCCESS);
-    // }
+    exit(EXIT_SUCCESS);
 }
