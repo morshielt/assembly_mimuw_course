fddc20901d8646106cbc8f74f76e846e3e48ef7a
maria <44531542+morshielt@users.noreply.github.com>
2020-11-27 19:03:14 +0100

    saving registers + added comments

--- game_of_life/game.asm
+++ game_of_life/game.asm
@@ -1,19 +1,13 @@
-%define    r r8
-%define    j r9
-%define    c r10
-%define    i r11
-; %define    cols r11                       ;
-; %define    rows r13                       ;
-%define    to_shift r14                      ;
-%define    from_shift r15
-; %define    tmp2 rax
-%define    neighbours rbx
-%define    table rcx ; current row
+%define    r r8                  ; current row
+%define    r_iter r9             ; row iterator (-1/0/1)
+%define    c r10                 ; current column
+%define    c_iter r11            ; column iterator (-1/0/1)
+%define    next_board r12        ; position in T where `next` board starts
+%define    curr_board r13        ; position in T where the current board starts
+%define    tmp r14               ; register used for calculating cell coordinates
+%define    neighbours rbx        ; number of alive neighbours of a cell
+%define    table rcx             ; holds address of T
 %define    steps rdi
-; %define    c rdx ; current column
-%define    tmp rax
-
-
 
 global start, run
 
@@ -21,187 +15,127 @@ section .bss
     COLS RESQ 1
     ROWS RESQ 1
     T RESQ 1
-    CTR RESQ 1
 
-; section .data
-    ; int ctr = 0;
-    ; CTR DB 0x0
+section .data
+    CTR DQ 0
 section .text
 start:
-    mov [COLS], rdi
+    mov [COLS], rdi               ; save params
     mov [ROWS], rsi
     mov [T], rdx
-    mov rax, 0
-    mov [CTR], rax
     ret
-    
 
-    ; lea rcx, [rel COLS]
-    ; mov rcx, rdi
-    ; lea rcx, [rel ROWS]
-    ; mov rcx, rsi
-    ; lea rcx, [rel T]
-    ; mov rcx, rdx
 
 run:
+    push    r8                   ; save registers
+    push    r9
+    push    r10
+    push    r11
+    push    r12
+    push    r13
+    push    r14
+    push    rbx
+    push   rbp
+    mov    rbp, rsp
 
-    ; push    r8
-    ; push    r9
-    ; push    r10
-    ; push    r12
-    ; push    r14
-    ; push    r15
-    ; push    rbx
-    ; push    rcx
-    ; push    rdx
-    ; push    rdi
-    ; push    rbp
-    ; mov    rbp, rsp                    ; restore register values
-
-
-    ; mov steps, rdi
-
-    ; xor r8, r8
-    ; xor r9, r9
-    ; xor r10, r10
-    ; xor r12, r12
-    ; xor r14, r14
-    ; xor r15, r15
-
-    ; xor rbx, rbx
-    ; xor rcx, rcx
-    ; xor rax, rax
     mov table, [T]
 
 step_loop:
-    ; for (size_t step = 0; step < steps; step++) {
-    mov tmp, [COLS] ; tmp = cols * rows
+    mov tmp, [COLS]              ; tmp = cols * rows
     imul tmp, [ROWS]
 
-    ;     int from = (ctr % 2) * cols * rows;
-    mov from_shift, [CTR]
-    imul from_shift, tmp
-    ; cmp from_shift, 0
-    ; jne next_step
+    mov curr_board, [CTR]        ; current boards shift
+    imul curr_board, tmp 
 
+    xor [CTR], BYTE 1            ; flip 0/1
+    mov next_board, [CTR]        ; calculated board shift
+    imul next_board, tmp
 
-
-    ;     int to = ((from + 1) % 2) * cols * rows;
-    xor [CTR], BYTE 1 ; flip 0/1
-    mov to_shift, [CTR]
-    imul to_shift, tmp
-
-
-
-
-    mov r, 0 ; current row column   rc = 0
+    mov r, 0                     ; current row = 0
 
 rows_loop:
-    mov c, 0
-    ;     for (size_t r = 0; r < rows; r++) {
-    ;         for (size_t c = 0; c < cols; c++) {
-
-    ;             int neighbours = 0;
+    mov c, 0                     ; current column = 0
 
 cols_loop:
-    mov neighbours, 0
+    mov neighbours, 0            ; current cell alive neighbours = 0
 
-    mov j, -1
-j_row_loop:
-    ;             for (int j = -1; j <= 1; j++) {
-        ;     (r + j >= 0 && r + j < rows) gud
-    
+    mov r_iter, -1               ; neighbouring row offset = -1
+neighbour_r_loop:                ; neighbouring rows (r-1, r, r+1)    
     mov tmp, r
-    add tmp, j
+    add tmp, r_iter
     cmp tmp, 0
-    jl skip_row         ; r + j < 0
+    jl next_neighbour_row        ; r + r_iter < 0 (out of bounds)
     cmp tmp, [ROWS]
-    jge skip_row        ; r + ; >= ROWS
+    jge next_neighbour_row       ; r + r_iter >= ROWS (out of bounds)
     
-    mov i, -1
-i_col_loop:
-    ;                 for (int i = -1; i <= 1; i++) {   
+    mov c_iter, -1               ; neighbouring column offset = -1
 
-
-    ; c + i >= 0 && c + i < cols
+neighbour_c_loop:                ; neighbouring columns (c-1, c, c+1)    
     mov tmp, c
-    add tmp, i
+    add tmp, c_iter
     cmp tmp, 0
-    jl skip_col     ; c + i < 0
+    jl next_neighbour_col        ; c + c_iter < 0 (out of bounds)
     cmp tmp, [COLS]
-    jge skip_col ; c + i >= COLS
+    jge next_neighbour_col       ; c + c_iter >= COLS (out of bounds)
 
-    mov tmp, r
-    add tmp, j
+    mov tmp, r                   ; calculating position of the neighbour in T
+    add tmp, r_iter
     imul tmp, [COLS]
     add tmp, c
-    add tmp, i
-    add tmp, from_shift
+    add tmp, c_iter
+    add tmp, curr_board
 
-    cmp [table + tmp], BYTE '1'
-    jne skip_col
-    inc neighbours
-    ;                     if (c + i >= 0 && c + i < cols && r + j >= 0 &&
-    ;                         r + j < rows) {
-    ;                         if (T[(r + j) * cols + (c + i) + from] == '1')
-    ;                             neighbours++;
-    ;                     }
-skip_col: 
-    inc i
-    cmp i, 1
-    jle i_col_loop
+    cmp [table + tmp], BYTE '1'  ; check `is neighbour alive?`
+    jne next_neighbour_col       ; not alive
+    inc neighbours               ; alive => neighbours++
 
-skip_row:
-    inc j
-    cmp j, 1
-    jle j_row_loop
+next_neighbour_col: 
+    inc c_iter
+    cmp c_iter, 1
+    jle neighbour_c_loop
 
-eval:
+next_neighbour_row:
+    inc r_iter
+    cmp r_iter, 1
+    jle neighbour_r_loop
+
+neighbours_counted:   
     mov tmp, r
     imul tmp, [COLS]
     add tmp, c
-    add tmp, from_shift
+    add tmp, curr_board
 
-    ;             if (T[r * cols + c + from] == '1') neighbours--;
-    cmp [table + tmp], BYTE '1' ; if we counted alive cell itself, subtract 1
-    jne eval_neighbours
+    cmp [table + tmp], BYTE '1'  ; if we counted in alive cell itself => neighbours--
+    jne eval_cell
     dec neighbours
 
-eval_neighbours:
-    cmp neighbours, 3
+eval_cell:
+    cmp neighbours, 3            ; 3 alive neighbours => cell alive
     je live
-    cmp neighbours, 2
+    cmp neighbours, 2            ; not 2 neighbours => cell dead
     jne die
-    cmp [table + tmp], BYTE '1'
+    cmp [table + tmp], BYTE '1'  ; 2 neibours and cell was alive => cell alive
     je live
 
-    die:
+    die:                         ; write cell to next board as dead
     mov tmp, r
     imul tmp, [COLS]
     add tmp, c
-    add tmp, to_shift
+    add tmp, next_board
 
     mov [table + tmp], BYTE '0'
     jmp next
 
-    live:
+    live:                        ; write cell to next board as alive
     mov tmp, r
     imul tmp, [COLS]
     add tmp, c
-    add tmp, to_shift
+    add tmp, next_board
 
     mov [table + tmp], BYTE '1'
 
-
-    ;             if (neighbours == 3 ||
-    ;                 (neighbours == 2 && T[r * cols + c + from] == '1')) {
-    ;                 T[r * cols + c + to] = '1';
-    ;             } else {
-    ;                 T[r * cols + c + to] = '0';
-    ;             }
-    ;         }
     
-next:
+next:                            ; go check next cell in column/row
     inc c
     cmp c, [COLS]
     jne cols_loop    
@@ -211,25 +145,20 @@ next:
     jne rows_loop
 
 next_step:
-   ; TODO: uncomment 
     dec steps
     cmp steps, 0
     jnz step_loop
 
+finish:
+    mov    rsp, rbp              ; restore register values
+    pop    rbp
+    pop    rbx
+    pop    r14
+    pop    r13
+    pop    r12
+    pop    r11
+    pop    r10
+    pop    r9
+    pop    r8
 
-
-
-;;;;;;;
-    ; mov    rsp, rbp                    ; restore register values
-    ; pop    rbp
-    ; pop    rdi
-    ; pop    rdx
-    ; pop    rcx
-    ; pop    rbx
-    ; pop    r15
-    ; pop    r14
-    ; pop    r12
-    ; pop    r10
-    ; pop    r9
-    ; pop    r8
     ret
